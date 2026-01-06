43029476d362c1692c91c92f632acb6fbe5b09df
vasanth <vasanthjayaraman6@gmail.com>
2026-01-06 19:09:53 +0530

    component creation - InputBox

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat' 
-    style={{backgroundImage:`url(https://images.pexels.com/photos/210607/pexels-photo-210607.jpeg?_gl=1*6urop3*_ga*MjAwMDY3Njg3Ny4xNzY0NDkzODIz*_ga_8JE65Q40S6*czE3NjY4MzAzMTMkbzQkZzEkdDE3NjY4MzAzNDAkajMzJGwwJGgw)`}}>
-      <h1 className='bg-red-300'>hello</h1>
-
-    </div>
-  )
+   
+      <div
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        style={{
+          backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+        }}
+      >
+        
+      </div>
+    
+  );
 }
 
-export default App
+export default App;
